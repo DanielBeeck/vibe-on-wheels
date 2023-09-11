c41f0c7ed96df83ccd2dc87453b6fe86252f2f41
Daniel <daniel.beck006@stud.fh-dortmund.de>
2023-09-12 00:09:01 +0200

    added daterangepicker

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <HeaderVue />
-  <mainLayout msg="Hit the Road."/>
+  <h1>Hit the Road.</h1>
+  <flatpickr />
+  <div>
+    <input ref="dateRangePicker" type="text" class="date-range-picker" />
+  </div>
+
 </template>
 
 <script>
-import mainLayout from './components/MainLayout.vue'
 import HeaderVue from './components/Header.vue'
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 
 export default {
   name: 'App',
   components: {
-    mainLayout,
-    HeaderVue
-  }
+    HeaderVue,
+    flatpickr
+  },
+  mounted() {
+    flatpickr(this.$refs.dateRangePicker, {
+      mode: 'range',
+      // Weitere Optionen hier
+    });
+  },
 }
 </script>
 
