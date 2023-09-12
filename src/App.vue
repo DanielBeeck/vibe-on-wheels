ea7e937d3b7afa554d82172c8aa658d80c2eebcb
Daniel <daniel.beck006@stud.fh-dortmund.de>
2023-09-13 01:15:15 +0200

    built general layout

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,76 @@
   <HeaderVue />
   <flatpickr />
   <div id="rentwindow">
-    <p>Miete ein Auto in deiner Nähe</p>
     <form>
-      <input id='pickupLoc' type="text" placeholder="Abholort">
-      <input id="pickupDate" ref="startDatePicker" type="text" class="date-range-picker" placeholder="Abholdatum"/>
-      <input id="bringbackDate" ref="endDatePicker" type="text" class="date-range-picker" placeholder="Rückgabedatum"/>
+      <p>Miete ein Auto in deiner Nähe</p>
+      <input id="pickupLoc" type="text" placeholder="Abholort" />
+      <input
+        id="pickupDate"
+        ref="startDatePicker"
+        type="text"
+        class="date-range-picker"
+        placeholder="Abholdatum"
+      />
+      <input
+        id="bringbackDate"
+        ref="endDatePicker"
+        type="text"
+        class="date-range-picker"
+        placeholder="Rückgabedatum"
+      />
     </form>
+    <img src="../src/assets/img/hyundai-motor-group-P45K3ewrATs-unsplash.jpg" alt="">
   </div>
-  <h1>Hit the Road.</h1>
+  <h2>Services</h2>
+  <div id="services">
+    <div class="service">
+      <img src="../src/assets/img/frontcar.png" alt="front of a car">
+      <h3>Flexibler Autoverleih</h3>
+      <p>Wählen Sie aus Autos zu verschiedenen anlässen.</p>
+    </div>
+    <div class="bookingproc">
+      <img src="../src/assets/img/dino-reichmuth.jpg" alt="">
+      <h3>Bequemer Buchungsprozess</h3>
+      <p>
+        Buchen sie ein Auto online über unser Portal oder kommen Sie zu uns in
+        die Filiale.
+      </p>
+    </div>
+    <div class="customersupport">
+      <img src="../src/assets/img/nate-johnston-.jpg" alt="">
+      <h3>Exzellenter Kundesupport</h3>
+      <p>
+        Unser freundliches und erfahrenes Team ist jederzeit bereit bei
+        Fragen zu Unterstützen.
+      </p>
+    </div>
+  </div>
+
+  <div id="recommendation">
+    <p>
+      Vibe on Wheels hat meine Mietwagenerfahrung außergewöhnlich
+      gemacht! Das Personal war freundlich, die Abwicklung verlief reibungslos,
+      und das Auto war in einem tollen Zustand. Vielen Dank für den problemlosen
+      und angenehmen Service. Sehr zu empfehlen!
+      <br><br>
+      - Sarah
+    </p>
+  </div>
+
+  <h2>About us</h2>
+  <p>
+    Vibe on Wheels ist eine Autovermietung in Bochum, NW. Unser Ziel ist eine
+    stressfreie Autovermietung mit einer großen Auswahl an gut gewarteten
+    Fahrzeugen für verschiedene Bedürfnisse. Wir bieten wettbewerbsfähige Preise
+    und flexible Mietoptionen. Unser Team strebt exzellenten Kundenservice an,
+    um Ihre Erfahrung zu optimieren. Danke für Ihre Wahl von Vibe on Wheels!
+  </p>
+  <FooterVue/>
 </template>
 
 <script>
 import HeaderVue from "./components/Header.vue";
+import FooterVue from "./components/Footer.vue"
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import "flatpickr/dist/themes/light.css";
@@ -22,6 +80,7 @@ export default {
   name: "App",
   components: {
     HeaderVue,
+    FooterVue,
   },
   mounted() {
     flatpickr(this.$refs.startDatePicker, {
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <hr>
+  <footer>
+    <div>
+      <h4>Produkte</h4>
+      <p>VOW+ Abo</p>
+      <p>Autovermietung</p>
+      <p>Transportervermietung</p>
+      <p>VOW Partnerprogramm</p>
+    </div>
+    <div>
+      <h4>Business</h4>
+      <p>Meine Firma registrieren</p>
+      <p>Geschäftsreisen und co.</p>
+      <p>Elektroautos für Geschäftskunden</p>
+    </div>
+    <div>
+      <h4>Über VibeOnWheels</h4>
+      <p>Über uns</p>
+      <p>Karriere</p>
+      <p>Partner werden</p>
+      <p>Blog</p>
+      <p>Impressum</p>
+    </div>
+    <div>
+      <h4>Hilfe und mehr</h4>
+      <p>FAQ</p>
+      <p>Kontakt</p>
+      <p>Stationsfinder</p>
+      <p>Hotelpartner</p>
+    </div>
+    <div>
+      <h4>Folgen Sie uns</h4>
+      <img src="../assets/icons/instagram.svg" alt="">
+      <img src="../assets/icons/twitter-square.svg" alt="">
+      <img src="../assets/icons/youtube-square.svg" alt="">
+      <img src="../assets/icons/linkedin-square.svg" alt="">
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footer-vue",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/../../main.scss";
+
+</style>
