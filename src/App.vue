691782af327381598c5c8bce217818a9e9a8473e
Daniel <daniel.beck006@stud.fh-dortmund.de>
2023-09-12 14:26:58 +0200

    added grid to the rentwindow

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,44 @@
 <template>
   <HeaderVue />
-  <h1>Hit the Road.</h1>
   <flatpickr />
-  <div>
-    <input ref="dateRangePicker" type="text" class="date-range-picker" />
+  <div id="rentwindow">
+    <p>Miete ein Auto in deiner Nähe</p>
+    <form>
+      <input id='pickupLoc' type="text" placeholder="Abholort">
+      <input id="pickupDate" ref="startDatePicker" type="text" class="date-range-picker" placeholder="Abholdatum"/>
+      <input id="bringbackDate" ref="endDatePicker" type="text" class="date-range-picker" placeholder="Rückgabedatum"/>
+    </form>
   </div>
-
+  <h1>Hit the Road.</h1>
 </template>
 
 <script>
-import HeaderVue from './components/Header.vue'
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-
+import HeaderVue from "./components/Header.vue";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/themes/light.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderVue,
-    flatpickr
   },
   mounted() {
-    flatpickr(this.$refs.dateRangePicker, {
-      mode: 'range',
-      // Weitere Optionen hier
+    flatpickr(this.$refs.startDatePicker, {
+      // Configuration for the start date picker
+      minDate: "today",
+      maxDate: new Date().fp_incr(14), // 14 days from now
+    });
+
+    flatpickr(this.$refs.endDatePicker, {
+      // Configuration for the end date picker
+      minDate: "today",
+      maxDate: new Date().fp_incr(14), // 14 days from now
     });
   },
-}
+};
 </script>
 
 <style lang="scss">
-  @import "@/../main.scss";
-#app {
-  text-align: center;
-}
+@import "@/../main.scss";
 </style>
